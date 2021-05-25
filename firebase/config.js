30ceb24fc68a714ab638d67f0f33db3ac64764bf
pratham173 <77920742+pratham173@users.noreply.github.com>
2021-05-25 07:36:21 +0530

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBfeKPvoxrOOkF5VG_iR2jri7KeUsDoXdI",
+    authDomain: "good-planet-d15d6.firebaseapp.com",
+    projectId: "good-planet-d15d6",
+    storageBucket: "good-planet-d15d6.appspot.com",
+    messagingSenderId: "537451603647",
+    appId: "1:537451603647:web:4ce3c7a83e3a7bb113880a",
+    measurementId: "G-1Q6B9F389D"
+  };
